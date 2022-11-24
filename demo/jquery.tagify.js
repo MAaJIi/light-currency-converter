19b29bb9f679398589d7c6c0255b45ebc6712bf9
MAaJIi <103310448+MAaJIi@users.noreply.github.com>
2022-11-24 08:54:29 +0000

    New translations django.po (Bulgarian)

--- demo/jquery.tagify.js
+++ demo/jquery.tagify.js
@@ -3,3 +3,7 @@
 
 (function ($) {
 	
+	$.widget("ui.tagify", {
+		options: {
+			delimiters: [13, 188, 44],          // what user can type to complete a tag in char codes: [enter], [comma]
+			outputDelimiter: ',',           // delimiter for tags in original input field
