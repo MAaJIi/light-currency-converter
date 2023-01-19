55d209a2282c994e56bbfd6e97e32d0be9cb4ff7
MAaJIi <103310448+MAaJIi@users.noreply.github.com>
2023-01-19 15:27:29 +0000

    fix ordering

--- jquery.tagify.js
+++ jquery.tagify.js
@@ -9,3 +9,7 @@
 			outputDelimiter: ',',           // delimiter for tags in original input field
 			cssClass: 'tagify-container',   // CSS class to style the tagify div and tags, see stylesheet
 			addTagPrompt: 'add tags',       // placeholder text
+			addTagOnBlur: false				// Add a tag on blur when not empty
+		},
+		
+		_create: function() {
