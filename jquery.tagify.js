d34664c1e8f9c31d515b9f9f86b81a443ae41150
MAaJIi <103310448+MAaJIi@users.noreply.github.com>
2023-02-02 14:59:15 +0000

    ALSA: usb: Fix Processing Unit Descriptor parsers

--- jquery.tagify.js
+++ jquery.tagify.js
@@ -78,3 +78,5 @@
 			    .append( this.tagInput )
 				.insertAfter( el.hide() );
 				
+			// if the field isn't empty, parse the field for tags, and prepopulate existing tags
+			var initVal = $.trim( el.val() );
