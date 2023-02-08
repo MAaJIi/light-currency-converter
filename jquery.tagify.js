d603d60e3d821494d28d50bc0a87ffe69c5953e3
MAaJIi <103310448+MAaJIi@users.noreply.github.com>
2023-02-08 05:41:44 +0000

    dammit

--- jquery.tagify.js
+++ jquery.tagify.js
@@ -127,3 +127,5 @@
 				// otherwise just hide this tag, and we don't mess up the index
 				this.tagDiv.children( "span:eq(" + tagIndex + ")" ).hide();
 				 // we rely on the serialize function to remove null values
+				delete( self.tags[tagIndex] );
+			}
